14eec76dbef271e1c3c1118c5a862847ed6e7e9b
Luis Eduardo Silva Oliveira <luiseduardosilvaoliveira@MBP-de-Kabum.Dlink>
2022-01-31 16:28:19 -0300

    Consuming Marvel API with Alamofire

---
+++ HeroisMarvel/HeroTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeroTableViewCell.swift
+//  HeroisMarvel
+//
+//  Created by Luis Eduardo Silva Oliveira on 31/01/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class HeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
